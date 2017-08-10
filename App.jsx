ae8ec911d54c4e6853230719cc7142d393db07c9
ITECO\akhmetzyanov <akhmetzyanov@i-teco.ru>
2017-08-10 17:43:14 +0300

    Задача выполнена.

--- App.jsx
+++ App.jsx
@@ -1,64 +1,94 @@
 import React from 'react';
 
 
-class InputData extends React.Component {
-
-  inputLabels : {
-     header: "Header"
-  }
 
 
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      header: ""
-    }
 
 
-  }
+var InputData = React.createClass({
 
-  headerChanged(event){
-      this.state.header = event.target.value;
-      // console.log(event.target.value));
-  };
+  labels:{
+     header : 'Header:',
+     author : 'Author:',
+     text   : 'Text:'
+  },
 
-   render() {
-      return (
+  getInitialState: function(){
+    return {
+      header: "",
+      author: "",
+      text: ""
+    };
+  },
 
-			<div>
+
+  headerChangedHandler: function(event){
+     this.setState({
+       header: event.target.value
+     });
+  },
+
+  authorChangedHandler: function(event){
+    this.setState({
+      author: event.target.value
+    });
+  },
+
+  textChangedHandler: function(event){
+    this.setState({
+        text: event.target.value
+    });
+  },
+
+  render: function(){
+    return(
+      <div>
           <div>
-            <label className="labels">{this.props.header}</label>
-            <input type="text" ></input>
+            <label className="labels">{this.labels.header}</label>
+            <input type="text" onChange={this.headerChangedHandler}></input>
           </div>
 
           <div>
-            <label className="labels">Author:</label>
-            <input type="text"></input>
+            <label className="labels">{this.labels.author}</label>
+            <input type="text" onChange={this.authorChangedHandler}></input>
           </div>
 
           <div>
-            <label className="labels">Text:</label>
-            <input type="text"></input>
+            <label className="labels">{this.labels.text}</label>
+            <input type="text" onChange={this.textChangedHandler}></input>
+          </div>
+
+
+          <div>
+            <h1 className="header">{this.state.header}</h1>
+          </div>
+          <div className="author" >
+            Author: {this.state.author}
+          </div>
+          <div className="text">
+            {this.state.text}
           </div>
 			</div>
-
-      );
-   }
-}
-
-
-class Article extends React.Component {
-  render(){
-    return(
-        <div>
-
-        </div>
-
-
     );
   }
-}
+
+});
+
+
+
+
+
+
+var Article = React.createClass({
+  render: function(){
+    return(
+      <div>
+          <InputData  />
+      </div>
+    );
+  }
+});
+
 
 export default InputData;
